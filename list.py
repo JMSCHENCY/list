9e87fd07f0cd1921528f1c8004cd794c977c3049
James Chen <jmschency@gmail.com>
2018-12-19 20:13:56 +0800

    Add new file

---
+++ list.py
@@ -0,0 +1,11 @@
+# list 清單
+
+a = ['Toyota', 'Honda'] # 空清單
+print(a)
+print(a[0])
+print(a[1])
+a.append('Audi') # 增加資料至list
+print(a)
+print(len(a))
+print('Audi' in a) # 是非題 True, False
+print('Benz' in a)
